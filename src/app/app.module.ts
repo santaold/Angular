fa6e168dad13263f06e0b262b47a0f60d8966c15
AleksandrTkachenko <santa.on@gmail.com>
2021-01-26 22:38:28 +0200

    lesson1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { CarsComponent } from './cars/cars.component';
+import {CarComponent} from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UsersComponent,
+    UserComponent,
+    CarComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule
